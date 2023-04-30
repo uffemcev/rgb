c018245f01d6dab46cd7749da271e53afcc3f240
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-05-01 02:18:10 +0700

    Delete cmd directory

--- cmd/uninstall.cmd
+++
@@ -1,5 +0,0 @@
-reg.exe add "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v "ScreenSaverIsSecure" /t REG_SZ /d "0" /f
-reg.exe delete "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v "ScreenSaverIsSecure" /f
-reg.exe delete "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v "ScreenSaveTimeOut" /f
-reg.exe add "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /f
-schtasks /delete /tn "RGB" /f
